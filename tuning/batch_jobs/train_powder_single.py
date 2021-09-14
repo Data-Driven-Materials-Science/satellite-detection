16c44eac14b0284ec5a16b9cd9939ec8f8175eb7
Stephen Price <stepheneprice1@gmail.com>
2021-09-13 23:38:13 -0400

    Adding in code to not calculate precision scores and save to file after a training is complete

--- tuning/batch_jobs/train_powder_single.py
+++ tuning/batch_jobs/train_powder_single.py
@@ -6,8 +6,13 @@ import numpy as np
 import os
 from pathlib import Path
 import pickle
+import skimage
 import skimage.io
 import sys
+import pandas as pd
+import pycocotools.mask as RLE
+import seaborn as sns
+
 
 ## detectron2
 from detectron2 import model_zoo
@@ -23,13 +28,21 @@ root = '../../../'
 ocean_images = root + '../../../../ocean/projects/dmr200021p/sprice/tuning/'
 sys.path.append(root)
 
-from ampis import data_utils, visualize
+from ampis import data_utils, visualize, analyze
+from ampis.applications import powder
+from ampis.structures import InstanceSet
+from ampis.visualize import display_iset
+
+#CONSTANTS
+#--------------------------------------------------------------
+EXPERIMENT_NAME = 'satellite' # can be 'particle' or 'satellite'
+NUM_ITERATIONS = 5000
+CHECKPOINT_NUM = 1000
+OUTPUT_FOLDER = 'batch_temp1'
+#--------------------------------------------------------------
+
 
 ##LOADING DATA
-
-EXPERIMENT_NAME = 'satellite' # can be 'particle' or 'satellite'
-OUTPUT_FOLDER = 'batch_temp1'
-
 json_path_train = Path(ocean_images,'satellite_auto_training_v1.6.json')  # path to training data
 json_path_val = Path(ocean_images, 'satellite_auto_validation_v1.2.json')  # path to training data
 assert json_path_train.is_file(), 'training file not found!'
@@ -76,11 +89,11 @@ cfg.DATASETS.TRAIN = (dataset_train,)  # dataset used for training model
 cfg.DATASETS.VALIDATION = (dataset_valid,)
 cfg.DATASETS.TEST = (dataset_train, dataset_valid)  # we will look at the predictions on both sets after training
 cfg.SOLVER.IMS_PER_BATCH = 1 # number of images per batch (across all machines)
-cfg.SOLVER.CHECKPOINT_PERIOD = 1000  # number of iterations after which to save model checkpoints
+cfg.SOLVER.CHECKPOINT_PERIOD = CHECKPOINT_NUM  # number of iterations after which to save model checkpoints
 cfg.MODEL.DEVICE='cuda'  # 'cpu' to force model to run on cpu, 'cuda' if you have a compatible gpu
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1 # Since we are training separate models for particles and satellites there is only one class output
 cfg.TEST.DETECTIONS_PER_IMAGE = 400 if EXPERIMENT_NAME == 'particle' else 250  # maximum number of instances that can be detected in an image (this is fixed in mask r-cnn)
-cfg.SOLVER.MAX_ITER = 5000  # maximum number of iterations to run during training
+cfg.SOLVER.MAX_ITER = NUM_ITERATIONS  # maximum number of iterations to run during training
                             # Increasing this may improve the training results, but will take longer to run (especially without a gpu!)
 
 # model weights will be downloaded if they are not present
@@ -101,30 +114,142 @@ trainer = DefaultTrainer(cfg)  # create trainer object from cfg
 trainer.resume_or_load(resume=False)  # start training from iteration 0
 trainer.train()  # train the model!
 
+
+
 #CREATING PREDICTOR
 model_checkpoints = sorted(Path(cfg.OUTPUT_DIR).glob('*.pth'))  # paths to weights saved druing training
 #cfg.DATASETS.TEST = (dataset_train, dataset_valid)  # predictor requires this field to not be empty
-cfg.MODEL.WEIGHTS = str(model_checkpoints[-4])  # use the last model checkpoint saved during training. If you want to see the performance of other checkpoints you can select a different index from model_checkpoints.
-predictor = DefaultPredictor(cfg)  # create predictor object
+for i in range(NUM_ITERATIONS/CHECKPOINT_NUM):
+    cfg.MODEL.WEIGHTS = str(model_checkpoints[-i-1])  # use the last model checkpoint saved during training. If you want to see the performance of other checkpoints you can select a different index from model_checkpoints.
+    predictor = DefaultPredictor(cfg)  # create predictor object
+    #SAVING RESULTS
+    results = []
+    for ds in cfg.DATASETS.VALIDATION:
+        print(f'Dataset: {ds}')
+        for dd in DatasetCatalog.get(ds):
+            print(f'\tFile: {dd["file_name"]}')
+            img = cv2.imread(dd['file_name'])  # load image
+            outs = predictor(img)  # run inference on image
+            
+            # format results for visualization and store for later
+            results.append(data_utils.format_outputs(dd['file_name'], ds, outs))
+
+            # visualize results
+            visualize.display_ddicts(outs, None, ds, gt=False, img_path=dd['file_name'])
+    
+    # save to disk
+    if i != 0:
+        title = 'results_checkpoint_0.pickle'
+    else:
+        title = 'results_checkpoint_' + (i * CHECKPOINT_NUM - 1) + ".pickle"
+    with open(Path(ocean_images, 'weights', OUTPUT_FOLDER, title), 'wb') as f:
+        pickle.dump(results, f)
+    
+    #CALCULATING SEGMENTATION SCORES
+    average_p = []
+    average_r = []
+    for i in range(7):
+        #Loading Ground Truth Labels
+        satellites_gt_path = Path('satellite_auto_validation_v1.2.json')
+        for path in [satellites_gt_path]:
+            assert path.is_file(), f'File not found : {path}'
+        satellites_gt_dd = data_utils.get_ddicts('via2', satellites_gt_path, dataset_class='train')
+        #Loading Prediction Labels
+        satellites_path = Path(ocean_images, 'weights', OUTPUT_FOLDER, title)
+        assert satellites_path.is_file()
+        with open(satellites_path, 'rb') as f:
+            satellites_pred = pickle.load(f)
+        iset_satellites_gt = [InstanceSet().read_from_ddict(x, inplace=False) for x in satellites_gt_dd]
+        iset_satellites_pred = [InstanceSet().read_from_model_out(x, inplace=False) for x in satellites_pred]
+        #Creating Instance Set Objects
+        iset_satellites_gt, iset_satellites_pred = analyze.align_instance_sets(iset_satellites_gt, iset_satellites_pred)
+        #Re-ordering instance sets to be concurrent
+        for gt, pred in zip(iset_satellites_gt, iset_satellites_pred):
+            pred.HFW = gt.HFW
+            pred.HFW_units = gt.HFW_units
+            print(f'gt filename: {Path(gt.filepath).name}\t pred filename: {Path(pred.filepath).name}')
+        #Creating Detection Scores
+        dss_satellites = [analyze.det_seg_scores(gt, pred, size=gt.instances.image_size)
+                        for gt, pred in zip(iset_satellites_gt, iset_satellites_pred)]
+        labels = []
+        counts = {'train': 0, 'validation': 0}
+        for iset in iset_satellites_gt:
+            counts[iset.dataset_class] += 1
+            labels.append(iset.filepath.name)
+        x=[*([1] * len(labels)), *([2] * len(labels))]
+        # y values are the bar heights
+
+        scores = [*[x['det_precision'] for x in dss_satellites],
+            *[x['det_recall'] for x in dss_satellites]]
+        labels = labels * 2
+        print('x: ', x)
+        print('y: ', [np.round(x, decimals=2) for x in scores])
+        #print('labels: ', labels)
+        fig, ax = plt.subplots(figsize=(6,3), dpi=150)
+        sns.barplot(x=x, y=scores, hue=labels, ax=ax)
+        ax.legend(bbox_to_anchor=(1,1))
+        ax.set_ylabel('detection score')
+        ax.set_xticklabels(['precision','recall'])
+        print("Average Precision Score: ", str(sum([*[x['det_precision'] for x in dss_satellites]])/len([*[x['det_precision'] for x in dss_satellites]])))
+        print("Average Precision Score: ", str(sum([*[x['det_recall'] for x in dss_satellites]])/len([*[x['det_recall'] for x in dss_satellites]])))
+        #Analyzing Prediction Scores on a pixel level
+        temp_p = []
+        temp_r = []
+        total_area = 1024*768
+        for instance in range(len(iset_satellites_pred)):
+            fp_area = 0
+            fn_area = 0
+            tp_area = 0
+            iset_satellites_pred[instance].compute_rprops(keys=['area'])
+            for i in dss_satellites[instance]['det_fp']:
+                try: 
+                    fp_area += int(iset_satellites_pred[instance].rprops['area'][i])
+                except:
+                    pass
+
+            for i in dss_satellites[instance]['det_fn']:
+                try: 
+                    fn_area += int(iset_satellites_pred[instance].rprops['area'][i])
+                except:
+                    pass
+
+            #print(dss_satellites[0]['seg_tp'])
+            for i in dss_satellites[instance]['det_tp']:
+                try: 
+                    tp_area += int(iset_satellites_pred[instance].rprops['area'][i[1]])
+                except:
+                    pass
+            print("Precision:", str(tp_area/(tp_area+fp_area)))
+            print('Recall:', str(tp_area/(tp_area+fn_area)))
+            temp_p.append(tp_area/(tp_area+fp_area))
+            temp_r.append(tp_area/(tp_area+fn_area))
+            print('---')
+        average_p.append(temp_p)
+        average_r.append(temp_r)
+        counter = 0   
+        for iset in iset_satellites_gt:
+            gt = iset_satellites_gt[counter]
+            pred = iset_satellites_pred[counter]
+            iset_det, colormap = analyze.det_perf_iset(gt, pred)
+            img = skimage.color.gray2rgb(skimage.io.imread(iset.filepath))
+            #display_iset(img, iset=iset_det)
+            counter += 1
+    file_name = 'single_train_output_it_' + i + '.txt'
+    with open(file_name, "a") as output:
+        output.write(str(average_p))
+    f = open(file_name, "a")
+    f.write('\n')
+    f.close()
+    with open(file_name, "a") as output:
+        output.write(str(average_p))
+    f = open(file_name, "a")
+    f.write('\n')
+    f.close()
 
 
 
-#SAVING RESULTS
-results = []
-for ds in cfg.DATASETS.VALIDATION:
-    print(f'Dataset: {ds}')
-    for dd in DatasetCatalog.get(ds):
-        print(f'\tFile: {dd["file_name"]}')
-        img = cv2.imread(dd['file_name'])  # load image
-        outs = predictor(img)  # run inference on image
-        
-        # format results for visualization and store for later
-        results.append(data_utils.format_outputs(dd['file_name'], ds, outs))
+    
 
-        # visualize results
-        visualize.display_ddicts(outs, None, ds, gt=False, img_path=dd['file_name'])
 
-# save to disk
-with open(Path(ocean_images, 'weights', OUTPUT_FOLDER, 'results.pickle.'), 'wb') as f:
-    pickle.dump(results, f)
+
 
