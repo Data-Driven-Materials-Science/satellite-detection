e85b35be306dceebb16a0fd1beeb482d2c2e3341
Stephen Price <sprice@exp-7-60.eth.cluster>
2022-01-19 11:11:45 -0800

    Added solar cell

---
+++ solarCells/batch_jobs/batch_test_deterministic1NonShared.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p GPU-small
+#SBATCH -t 1:00:00
+#SBATCH --gres=gpu:1
+CUBLAS_WORKSPACE_CONFIG=:16:8 python3 train_powder_deterministic1.py
+
+./gpua.out
---
+++ solarCells/batch_jobs/stage_2/Turn_3/b_t_007.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p GPU-shared
+#SBATCH -t 10:00:00
+#SBATCH --gres=gpu:1
+
+python3 train_powder_LR-0_007_T3.py
+
+./gpua.out
